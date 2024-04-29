fab0ef3e688a09377f82abe581de1b404a7717cd
madeflyoke <sano555000@gmail.com>
2024-04-29 16:51:23 +0300

    fix: player controller slopes fixes

--- Assets/Main/Player/PlayerMovementComponent.cs
+++ Assets/Main/Player/PlayerMovementComponent.cs
@@ -17,6 +17,7 @@ namespace Main.Player
         [SerializeField] private float _jumpForce = 1.5f;
         [SerializeField] private float _airMultiplier = 0.3f;
         [SerializeField] private float _gravity;
+        [SerializeField] private float _slopesGravityMultiplier = 15f;
 
         private float _horizontalInput;
         private float _verticalInput;
@@ -64,27 +65,42 @@ namespace Main.Player
             ValidateSpeed();
             // Debug.LogWarning(1/Time.deltaTime);
         }
+
         
         private void Move()
         {
             _cameraHolder.GetCorrectedVectors(out Vector3 forwardVector, out Vector3 rightVector);
 
+            if (_surfaceChecker.ValidateSlope()==false)
+            {
+                _rb.AddForce(Vector3.down*_slopesGravityMultiplier, ForceMode.Impulse);
+            }
+            
             var velocity =  ( forwardVector * _verticalInput +rightVector * _horizontalInput).normalized
                             *_velocityMultiplier;
             
             if (_surfaceChecker.IsGrounded==false || _isInJump)
-            {
-                velocity *= _airMultiplier;
-                
-                float yVelocityByDrag = _rb.velocity.y * _rb.drag;
-                float dragCoef = (1 - Time.deltaTime * _rb.drag);
-                
-                _rb.AddForce(new Vector3 (0, ((yVelocityByDrag + _gravity) / dragCoef) * _rb.mass, 0));
-            }
+            { 
+                SimulateGravity();
 
-            var finalVelocity = Vector3.ProjectOnPlane(velocity, _surfaceChecker.CurrentNormal);
-            _rb.AddForce(finalVelocity,ForceMode.Impulse);
+                velocity = new Vector3(velocity.x * _airMultiplier, velocity.y, velocity.z*_airMultiplier);
+            }
+            else
+            {
+                velocity = _surfaceChecker.GetNormalProjectedVector(velocity);
+            }
+            
+            _rb.AddForce(velocity,ForceMode.Impulse);
         }
+
+        private void SimulateGravity()
+        {
+            float yVelocityByDrag = _rb.velocity.y * _rb.drag;
+            float dragCoef = (1 - Time.deltaTime * _rb.drag);
+                
+            _rb.AddForce(Vector3.up * ((yVelocityByDrag + _gravity) / dragCoef * _rb.mass));
+        }
+
 
         private void ValidateSpeed()
         {
--- Assets/Main/Player/SurfaceChecker.cs
+++ Assets/Main/Player/SurfaceChecker.cs
@@ -12,7 +12,6 @@ namespace Main.Player
     public class SurfaceChecker : MonoBehaviour
     {
         public bool IsGrounded { get;private set; }
-        public Vector3 CurrentNormal { get; private set; }
 
         public event Action AboveGroundEvent;
         public event Action LandedGroundEvent;
@@ -21,15 +20,31 @@ namespace Main.Player
         [SerializeField] private Transform _bottomPoint;
         [SerializeField] private float _distance;
         [SerializeField] private LayerMask _groundMask;
+        [SerializeField] private float _slopeAngleLimit;
+        [SerializeField] private Collider _movementCollider;
         private IDisposable _raycastChecker;
+        private IDisposable _enterCollidersChecker;
+        private IDisposable _exitCollidersChecker;
+        private Vector3 _currentNormal = Vector3.up;
+        private List<Collider> _currentGrounds;
+        private Collider _suitableGround;
 
         private void Start()
         {
             AboveGroundEvent?.Invoke();
+            
+            InitGroundsCollector();
+            InitGroundChecker();
+        }
+
+        private void InitGroundChecker()
+        {
             _raycastChecker = this.UpdateAsObservable().Subscribe(x =>
             {
-                var isGrounded = Physics.Raycast(_bottomPoint.position, Vector3.down, out RaycastHit hit, _distance, _groundMask);
-                CurrentNormal = hit.normal;
+                Ray ray2 = new Ray(_bottomPoint.position, ValidateSlope()?-_currentNormal: Vector3.down);
+                
+                var isGrounded = Physics.Raycast(ray2, _distance, _groundMask);
+                
                 if (IsGrounded!=isGrounded)
                 {
                     IsGrounded = isGrounded;
@@ -42,31 +57,83 @@ namespace Main.Player
                         AboveGroundEvent?.Invoke();
                     }
                 }
-                // Debug.LogWarning(IsGrounded);
-
-            });
+            }).AddTo(this);
         }
 
+        private void InitGroundsCollector()
+        {
+            _currentGrounds = new List<Collider>();
+            _enterCollidersChecker = _movementCollider.OnCollisionEnterAsObservable().RepeatUntilDisable(transform).Subscribe(collision =>
+            {
+                if (_currentGrounds.Contains(collision.collider)==false)
+                {
+                    _currentGrounds.Add(collision.collider);
+                    SetSuitableGround();
+                }
+            }).AddTo(this);
+            
+            _exitCollidersChecker = _movementCollider.OnCollisionExitAsObservable().RepeatUntilDisable(transform).Subscribe(collision =>
+            {
+                if (_currentGrounds.Contains(collision.collider))
+                {
+                    _currentGrounds.Remove(collision.collider);
+                    SetSuitableGround();
+                }
+            }).AddTo(this);
+        }
+
+        private void SetSuitableGround()
+        {
+            if (_currentGrounds.Count != 0)
+            {
+                _suitableGround = _currentGrounds.OrderByDescending(x => Vector3.Dot(x.transform.up, Vector3.up)).First();
+                Debug.LogWarning("SELECTED NEW GROUND, DOT: "+Vector3.Dot(_suitableGround.transform.up, Vector3.up));
+                _currentNormal = _suitableGround.transform.up;
+            }
+        }
+        
         public bool ValidateSpaceAbove()
         {
             return Physics.Raycast(_topPoint.position, Vector3.up, _distance)==false;
         }
 
+        public bool ValidateSlope()
+        {
+            return Vector3.Angle(Vector3.up, _currentNormal)<=_slopeAngleLimit;
+        }
+        
+        public Vector3 GetNormalProjectedVector(Vector3 dir)
+        {
+            var finalVector = Vector3.ProjectOnPlane(dir, _currentNormal);
+            
+#if UNITY_EDITOR
+            if (dir != Vector3.zero)
+            {
+                EDITOR_normalDir = finalVector;
+            }
+#endif
+            return finalVector;
+        }
+
         private void OnDisable()
         {
             _raycastChecker?.Dispose();
+            _enterCollidersChecker?.Dispose();
+            _exitCollidersChecker?.Dispose();
         }
 
 #if UNITY_EDITOR
         [SerializeField] private List<Collider> EDITOR_physicsColliders;
-
+        [SerializeField] private float EDITOR_distanceBetweenPoints;
+        private Vector3 EDITOR_normalDir;
+        
         [Button]
-        private void SetTopBotPoints()
+        private void SetupSurfaceAdditionals()
         {
             if (EDITOR_physicsColliders != null && EDITOR_physicsColliders.Count != 0)
             {
-                var minY = EDITOR_physicsColliders.Min(x => x.bounds.min.y) + 0.2f;
-                var maxY = EDITOR_physicsColliders.Max(x => x.bounds.max.y) - 0.2f;
+                var minY = EDITOR_physicsColliders.Min(x => x.bounds.min.y) + EDITOR_distanceBetweenPoints;
+                var maxY = EDITOR_physicsColliders.Max(x => x.bounds.max.y) - EDITOR_distanceBetweenPoints;
                 
                 _topPoint.position = new Vector3(transform.position.x, maxY, transform.position.z);
                 _bottomPoint.position = new Vector3(transform.position.x, minY, transform.position.z);
@@ -75,8 +142,20 @@ namespace Main.Player
 
         private void OnDrawGizmos()
         {
+            Handles.color = Color.red;
+            if (EDITOR_normalDir!=Vector3.zero)
+            {
+                Handles.DrawLine(transform.position, transform.position+EDITOR_normalDir, 3f);
+                Gizmos.color = Color.yellow;   
+                Gizmos.DrawLine(_bottomPoint.position, _bottomPoint.position+ (ValidateSlope()?-_currentNormal: Vector3.down) *_distance);
+            }
+            else
+            {
+                Handles.color = Color.yellow;
+                Handles.DrawLine(_bottomPoint.position, _bottomPoint.position + (-_bottomPoint.up.normalized * _distance), 1f);
+            }
+            
             Handles.color = Color.yellow;
-            Handles.DrawLine(_bottomPoint.position, _bottomPoint.position + (-_bottomPoint.up.normalized * _distance), 1f);
             Handles.DrawLine(_topPoint.position, _topPoint.position + (_topPoint.up.normalized * _distance), 1f);
         }
         
