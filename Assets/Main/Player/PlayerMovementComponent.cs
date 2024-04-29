38e6ac9e5fb64605dcabf42f465be447600392d0
madeflyoke <sano555000@gmail.com>
2024-04-29 21:56:33 +0300

    fix: player movement fix

--- Assets/Main/Player/PlayerMovementComponent.cs
+++ Assets/Main/Player/PlayerMovementComponent.cs
@@ -21,27 +21,23 @@ namespace Main.Player
 
         private float _horizontalInput;
         private float _verticalInput;
-        private float _defaultDrag;
-        private bool _isInJump=true;
+        private bool _preparingToJump=true;
 
         private float deltatime;
         
         private void Awake()
         {
-            _defaultDrag = _rb.drag;
             Application.targetFrameRate = 60;
         }
         
         private void OnEnable()
         {
             _surfaceChecker.AboveGroundEvent += OnAboveGround;
-            _surfaceChecker.LandedGroundEvent += OnLandedGround;
         }
 
         private void OnDisable()
         {
             _surfaceChecker.AboveGroundEvent -= OnAboveGround;
-            _surfaceChecker.LandedGroundEvent -= OnLandedGround;
         }
         
         private void FixedUpdate()
@@ -54,7 +50,7 @@ namespace Main.Player
             _horizontalInput = Input.GetAxisRaw("Horizontal");
             _verticalInput = Input.GetAxisRaw("Vertical");
             
-            if (Input.GetKey(KeyCode.Space) && _isInJump==false)
+            if (Input.GetKey(KeyCode.Space) && _surfaceChecker.IsTouchingGround && _preparingToJump==false)
             {
                 if (_surfaceChecker.ValidateSpaceAbove())
                 {
@@ -71,7 +67,7 @@ namespace Main.Player
         {
             _cameraHolder.GetCorrectedVectors(out Vector3 forwardVector, out Vector3 rightVector);
 
-            if (_surfaceChecker.ValidateSlope()==false)
+            if (_surfaceChecker.ValidateSlope()==false && _surfaceChecker.IsTouchingGround)
             {
                 _rb.AddForce(Vector3.down*_slopesGravityMultiplier, ForceMode.Impulse);
             }
@@ -79,7 +75,7 @@ namespace Main.Player
             var velocity =  ( forwardVector * _verticalInput +rightVector * _horizontalInput).normalized
                             *_velocityMultiplier;
             
-            if (_surfaceChecker.IsGrounded==false || _isInJump)
+            if (_surfaceChecker.IsTouchingGround==false || _preparingToJump)
             { 
                 SimulateGravity();
 
@@ -100,8 +96,7 @@ namespace Main.Player
                 
             _rb.AddForce(Vector3.up * ((yVelocityByDrag + _gravity) / dragCoef * _rb.mass));
         }
-
-
+        
         private void ValidateSpeed()
         {
             Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
@@ -115,21 +110,16 @@ namespace Main.Player
 
         private void Jump()
         {
-            _isInJump = true;
+            _preparingToJump = true;
 
             _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
             _rb.AddForce(Vector3.up * _velocityMultiplier * _jumpForce, ForceMode.Impulse);
+            _surfaceChecker.ProcessingJumpCheck();
         }
 
         private void OnAboveGround()
         {
-            
-        }
-
-        private void OnLandedGround()
-        {
-            _rb.drag = _defaultDrag;
-            _isInJump = false;
+            _preparingToJump = false;
         }
     }
 }
