d57e09e7d48f14e0ed3e129b3f29b49902b76989
madeflyoke <sano555000@gmail.com>
2024-05-01 17:28:34 +0300

    fix: movement fixes

--- Assets/Main/Player/PlayerMovementComponent.cs
+++ Assets/Main/Player/PlayerMovementComponent.cs
@@ -11,38 +11,42 @@ namespace Main.Player
         [SerializeField] private PlayerSurfaceHelperComponent _playerSurfaceHelperComponent;
         [SerializeField] private Rigidbody _rb;
         [Header("Movement")]
-        [SerializeField] private float _maxMovementSpeed = 10f;
+        [SerializeField] private float _maxVelocity = 10f;
         [SerializeField] private float _velocityMultiplier = 6f;
         [Header("Jumping")]
-        [SerializeField] private float _jumpForce = 1.5f;
-        [SerializeField] private float _airMultiplier = 0.3f;
-        [SerializeField] private float _gravity;
-        [SerializeField] private float _steepSlopesGravityMultiplier = 15f;
+        [SerializeField] private float _jumpForce = 2f;
+        [SerializeField] private float _airMultiplier = 0.8f;
+        [SerializeField] private float _gravity = -15f;
+        [SerializeField] private float _steepSlopesGravityMultiplier = 40f;
 
         private float _horizontalInput;
         private float _verticalInput;
+        
         private bool _preparingToJump=true;
-        private bool InJumpingProcess => _playerSurfaceHelperComponent.IsTouchingGround == false || _preparingToJump;
+        private bool _jumpKeyPressed;
+        private Vector3 _currentCalculatedVelocity;
+        private float _lastJumpTime;
         
         private void Awake()
         {
             Application.targetFrameRate = 60;
         }
-        
-        private void OnEnable()
-        {
-            _playerSurfaceHelperComponent.AboveGroundedEvent += OnAboveGrounded;
-        }
 
-        private void OnDisable()
+        private void FixedUpdate() //order matters
         {
-            _playerSurfaceHelperComponent.AboveGroundedEvent -= OnAboveGrounded;
-        }
-        
-        private void FixedUpdate()
-        {
-            Move();
-            ValidateSpeed();
+            _currentCalculatedVelocity = Vector3.zero;
+
+            var isGrounded = RetrieveGroundedInfo();
+            
+            TrySetGroundDistance(isGrounded); //most important method
+            
+            HandleMovementVelocity(isGrounded);
+            
+            HandleJumping(isGrounded);
+            
+            //CheckLandedGroundInfo(); //if need to set Y to 0 on landed
+
+            Move(_currentCalculatedVelocity,isGrounded);
         }
 
         private void Update()
@@ -50,103 +54,141 @@ namespace Main.Player
             _horizontalInput = Input.GetAxisRaw("Horizontal");
             _verticalInput = Input.GetAxisRaw("Vertical");
             
-            if (Input.GetKey(KeyCode.Space) && InJumpingProcess==false)
+            if (Input.GetKey(KeyCode.Space))
             {
-                Jump();
+                _jumpKeyPressed = true;
             }
         }
 
-        private void Move()
+        private void Move(Vector3 velocity, bool isGrounded)
         {
-            _playerSurfaceHelperComponent.CastChecker();
-            
-            _cameraHolder.GetCorrectedVectors(out Vector3 forwardVector, out Vector3 rightVector);
-            
-            var velocity =  ( forwardVector * _verticalInput +rightVector * _horizontalInput).normalized
-                            *_velocityMultiplier;
-            
-            if (InJumpingProcess)
+            velocity = ValidateVelocity(velocity, isGrounded);
+            _rb.AddForce(velocity,ForceMode.Impulse);
+        }
+
+        private void HandleMovementVelocity(bool isGrounded)
+        {
+            var velocity = GetInputNormalizedDirection() * _velocityMultiplier;
+
+            if (isGrounded==false)
             {
                 velocity += SimulateGravity();
-
                 velocity = new Vector3(velocity.x * _airMultiplier, velocity.y, velocity.z*_airMultiplier);
             }
             else
             {
                 velocity = _playerSurfaceHelperComponent.GetNormalProjectedVector(velocity);
-
-                if (_playerSurfaceHelperComponent.ValidateSlope()==false)
+                
+                if (_playerSurfaceHelperComponent.ValidateSlope()==false) //too steep slope
                 {
-                    velocity = new Vector3(velocity.x * _airMultiplier/2, velocity.y, velocity.z*_airMultiplier/2);
-
+                    velocity = new Vector3(velocity.x * _airMultiplier/3, velocity.y, velocity.z*_airMultiplier/3);
+            
                     velocity += _playerSurfaceHelperComponent.GetNormalProjectedVector(-transform.up, true)
                                 * _steepSlopesGravityMultiplier*_velocityMultiplier *Time.fixedDeltaTime;
                 }
-                else
-                {
-                    var vel = _rb.velocity;
-                    vel.y = 0f;
-                    _rb.velocity = vel;
-                }
-                
-                velocity += TryFloatPositionRb();
             }
 
-            _rb.AddForce(velocity,ForceMode.Impulse);
+            _currentCalculatedVelocity += velocity;
         }
-        
-        private Vector3 TryFloatPositionRb()
+
+        private bool RetrieveGroundedInfo()
         {
-            if (InJumpingProcess==false)
+            if (_preparingToJump)
             {
-                var diff = _playerSurfaceHelperComponent.GetGroundDistanceDifference();
-                if (Mathf.Approximately(diff,0)==false)
+                if (IsJumpPreparingTimeOut() || _playerSurfaceHelperComponent.JustAboveGround)
                 {
-                    return transform.up * (diff / Time.fixedDeltaTime);
+                    _preparingToJump = false;
                 }
             }
-            return Vector3.zero;
+            
+            return _playerSurfaceHelperComponent.CastChecker() && _preparingToJump==false;
+        }
+
+        private void CheckLandedGroundInfo()
+        {
+            if (_playerSurfaceHelperComponent.JustTouchedGround)
+            {
+                CompleteResetY();
+            }
+        }
+        
+        private void HandleJumping(bool isGrounded)
+        {
+            if (_jumpKeyPressed)
+            {
+                _jumpKeyPressed = false;
+                
+                if (isGrounded && _playerSurfaceHelperComponent.ValidateSlope())
+                {
+                    _preparingToJump = true;
+                    Debug.LogWarning("JUMPED");
+                    CheckLandedGroundInfo();
+                    _currentCalculatedVelocity += (Vector3.up * _velocityMultiplier * _jumpForce) - (Vector3.up*_rb.velocity.y);
+                    _lastJumpTime = Time.time;
+                }
+            }
+        }
+        
+        private bool IsJumpPreparingTimeOut() //safety check  
+        {
+            if (Time.time-(_lastJumpTime+1f)<=0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void CompleteResetY()
+        {
+            _currentCalculatedVelocity.y = 0f;
+            var velocity = _rb.velocity;
+            velocity = new Vector3(velocity.x, 0f, velocity.z);
+            _rb.velocity = velocity;
+        }
+
+        private Vector3 GetInputNormalizedDirection()
+        {
+            _cameraHolder.GetCorrectedVectors(out var forwardVector, out var rightVector);
+            
+            return (forwardVector * _verticalInput +rightVector * _horizontalInput).normalized;
+        } 
+        
+        private void TrySetGroundDistance(bool isGrounded)
+        {
+            if (isGrounded==false || _playerSurfaceHelperComponent.ValidateSlope()==false)
+            {
+                return;
+            }
+            var diff = _playerSurfaceHelperComponent.GetGroundDistanceDifference();
+
+            if (diff>0.03f)
+            {
+                var velocity = _rb.velocity;
+                velocity = new Vector3(velocity.x, diff / Time.fixedDeltaTime, velocity.z); //use this instead of AddForce to get physics-instant effect 
+                _rb.velocity = velocity;
+            }
         }
 
         private Vector3 SimulateGravity()
         {
-            float yVelocityByDrag = _rb.velocity.y * _rb.drag;
-            float dragCoef = (1 - Time.deltaTime * _rb.drag);
+            var yVelocityByDrag = _rb.velocity.y * _rb.drag;
+            var dragCoef = (1 - Time.deltaTime * _rb.drag);
 
             return Vector3.up * ((yVelocityByDrag + _gravity) / dragCoef * _rb.mass)*Time.fixedDeltaTime;
         }
         
-        private void ValidateSpeed()
+        private Vector3 ValidateVelocity(Vector3 velocity, bool isGrounded)
         {
-            Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+            var flatVel = new Vector3(velocity.x, 0f, velocity.z);
             
-            if (flatVel.magnitude>_maxMovementSpeed)
+            if (flatVel.magnitude>_maxVelocity)
             {
-                Vector3 limitedVel = flatVel.normalized * _maxMovementSpeed;
-                _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
+                var limitedVel = flatVel.normalized * _maxVelocity*(isGrounded?_airMultiplier:1f); // worth it?
+                return new Vector3(limitedVel.x, velocity.y, limitedVel.z);
             }
-        }
 
-        private void Jump() //TODO Restrict jump while sloping
-        {
-            _preparingToJump = true;
-
-            _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
-            _rb.AddForce(Vector3.up * _velocityMultiplier * _jumpForce, ForceMode.Impulse);
-            _playerSurfaceHelperComponent.ProcessingJumpCheck();
-        }
-
-        private void OnAboveGrounded()
-        {
-            _preparingToJump = false;
-        }
-
-        private void CheckJumpInfo() //remove constant AboveGround event
-        {
-            if (_playerSurfaceHelperComponent)
-            {
-                
-            }
+            return velocity;
         }
     }
 }
--- Assets/Main/Player/PlayerSurfaceHelperComponent.cs
+++ Assets/Main/Player/PlayerSurfaceHelperComponent.cs
@@ -8,66 +8,64 @@ namespace Main.Player
 {
     public class PlayerSurfaceHelperComponent : MonoBehaviour
     {
-        public bool IsTouchingGround { get;private set; }
 
-        public event Action AboveGroundedEvent;
-        public event Action LandedEvent;
+        public bool JustAboveGround { get; private set; }
+        public bool JustTouchedGround { get; private set; }
         
         [SerializeField] private Transform _groundCheckerRaycastTr;
         [SerializeField] private float _distance;
         [SerializeField] private LayerMask _groundMask;
         [SerializeField] private float _slopeAngleLimit;
         private Vector3 _currentNormal = Vector3.up;
-        private CancellationTokenSource _cts;
         private float _hitDistance;
+        
+        private bool _isTouchingGround;
+
         
         public bool CastChecker()
         {
+            ResetFlags();
+            
             Ray ray = new Ray(_groundCheckerRaycastTr.position,Vector3.down);
 
-            if (Physics.Raycast(ray, out RaycastHit hit,_distance, _groundMask))
+            if (Physics.Raycast(ray, out RaycastHit hit, _distance, _groundMask))
             {
-                IsTouchingGround = true;
+                if (_isTouchingGround==false)
+                {
+                    JustTouchedGround = true;
+                }
                 _currentNormal = hit.normal;
                  _hitDistance = hit.distance;
-                 return true;
-                 //_hitDistance = Mathf.Abs(_groundCheckerRaycastTr.position.y - hit.point.y);
+                 
+                 _isTouchingGround = true;
             }
             else
             {
-                IsTouchingGround = false;
-                AboveGroundedEvent?.Invoke();
-                _currentNormal = Vector3.up;
-                _hitDistance = 0f;
+                if (_isTouchingGround)
+                {
+                    JustAboveGround = true;
+                }
+                
+                _isTouchingGround = false;
             }
 
-            return false;
-            Debug.LogWarning(IsTouchingGround);
+            return _isTouchingGround;
+        }
+
+        private void ResetFlags()
+        {
+            JustTouchedGround = false;
+            JustAboveGround = false;
+            _currentNormal = Vector3.up;
+            _hitDistance = 0f;
         }
 
         public float GetGroundDistanceDifference()
         {
-            var diff = (_distance*0.9f) - _hitDistance;
+            var diff = (_distance *0.9f - _hitDistance);
             return diff ;
         }
-        
-        public async void ProcessingJumpCheck() //safety check  
-        {
-            _cts = new CancellationTokenSource();
-            float safetySec = 1f;
-            while (safetySec > 0)
-            {
-                var canceled = await UniTask.WaitForFixedUpdate(_cts.Token).SuppressCancellationThrow();
-                if (canceled || IsTouchingGround==false)
-                {
-                    return;
-                }
 
-                safetySec -= Time.fixedDeltaTime;
-            }
-            AboveGroundedEvent?.Invoke();
-        }
-        
         public bool ValidateSlope()
         {
             return Vector3.Angle(Vector3.up, _currentNormal)<=_slopeAngleLimit;
@@ -84,11 +82,6 @@ namespace Main.Player
             }
 #endif
             return finalVector;
-        }
-
-        private void OnDisable()
-        {
-            _cts?.Dispose();
         }
 
 #if UNITY_EDITOR
